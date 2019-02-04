49a26bcaba22c88305d81f9ba6a5a7864379ee83
MaximKrasikov <karasy77@yandex.ru>
2019-02-05 00:55:51 +0300

    added registration

--- Netcracker/src/main/java/com/entities/Role.java
+++ Netcracker/src/main/java/com/entities/Role.java
@@ -3,6 +3,11 @@ package com.entities;
 /**
  * Created by Admin on 04.02.2019.
  */
+/*будет определять роли пользователя*/
 public enum Role {
-    USER;
+    ADMIN,
+    USER,
+    ANONYMOUS;
+    Role(){
+    }
 }
